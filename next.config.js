503c5408cbbdde38f5136b2d51624fa6bb861a36
Ben McLoughlin <benmcl@shaw.ca>
2022-07-07 12:18:03 -0700

    added redirects to next.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,26 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    headers: [
-        {
-            source: '/.well-known/apple-app-site-association',
-            headers: [
-                {
-                    key: 'Content-Type',
-                    value: 'application/json'
-                }
-            ]
-        }
-    ]
+    async headers() {
+        return [
+            {
+                source: '/.well-known/apple-app-site-association',
+                headers: [
+                    {
+                        key: 'Content-Type',
+                        value: 'application/json'
+                    }
+                ]
+            }
+        ];
+    },
+    async redirects() {
+        return [
+            {
+                source: '/.well-known/apple-app-site-association',
+                destination: '/api/apple-app-site-association',
+                permanent: true
+            }
+        ];
+    }
 };
