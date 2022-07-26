d78c460f052bd2ad7b1b9586ae118c408f1e9976
Ben McLoughlin <benmcl@shaw.ca>
2022-07-26 11:09:54 -0700

    switched aasa file to using sh app

--- pages/api/apple-app-site-association.js
+++ pages/api/apple-app-site-association.js
@@ -1,3 +1,4 @@
+/*
 export default function handler(req, res) {
     res.status(200).json({
         applinks: {
@@ -21,6 +22,34 @@ export default function handler(req, res) {
         },
         webcredentials: {
             apps: ['QLN6949UL2.com.universal-links-test.app']
+        }
+    });
+}
+*/
+
+export default function handler(req, res) {
+    res.status(200).json({
+        applinks: {
+            details: [
+                {
+                    appIDs: ['QLN6949UL2.com.shophopper.app'],
+                    components: [
+                        {
+                            '#': 'no_universal_links',
+                            exclude: true,
+                            comment:
+                                'Matches any URL whose fragment equals no_universal_links and instructs the system not to open it as a universal link'
+                        },
+                        {
+                            '/': '/shophopper-app/*',
+                            comment: 'Matches any URL whose path starts with /shophopper/'
+                        }
+                    ]
+                }
+            ]
+        },
+        webcredentials: {
+            apps: ['QLN6949UL2.com.shophopper.app']
         }
     });
 }
